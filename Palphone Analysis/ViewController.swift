b0a5221a391ed2335b4fc637453b05a8872fb8a2
palphone ios <palphoneios@palphones-Mini.lan>
2023-12-03 19:22:50 +0330

    feat:add welcomViewController

--- Palphone Analysis/ViewController.swift
+++ Palphone Analysis/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "Palphone Analysis"
+        for letter in titleText {
+            print("-")
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
 
 
